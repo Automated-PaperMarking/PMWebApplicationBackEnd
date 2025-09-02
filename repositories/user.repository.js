da44a02130e5ce5f5116fa45464dbfaa84a221b1
H.L.D.U.G Perera <138327662+UmeshGayashan@users.noreply.github.com>
2025-09-02 06:15:42 +0530

    update user table and create user repository

---
+++ repositories/user.repository.js
@@ -0,0 +1,41 @@
+import supabase from '../supabaseClient.js';
+import bcrypt from 'bcryptjs';
+
+const userRepository = {
+  insert_user : async (userData) => {
+    try {
+      // Password Hashing
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+      //  salt round of 10, which is secure and commonly used
+
+      const { data, error } = await supabase.rpc('insert_user', {
+        user_name: userData.name,
+        user_role: userData.role,
+        user_username: userData.username,
+        user_password: hashedPassword,
+        user_email: userData.email,
+        student_batch: userData.batch
+      });
+
+      if (error) throw error;
+      return data;
+    } catch (error) {
+      console.error('Repository Error:', error);
+      throw error;
+    }
+  },
+  exportUser : async ()=> {
+    try{
+      const { data, error } = await supabase
+        .from('Users')
+        .select('UserID, Name, Role, UserName, Email, Batch');
+      if (error) throw error;
+      return data;
+    }catch(error){
+      console.error('Repository Error:', error);
+      throw error;
+    }
+  }
+};
+
+export {studentRepository};
